11647ca20e7e99c2d9b8cf3de5095ef2deda01fa
Semptum <andre.kalouguine.ak@gmail.com>
2018-02-07 15:35:14 +0100

    Maintenant ça marche pas

--- PlanetsOnEllipses.py
+++ PlanetsOnEllipses.py
@@ -14,16 +14,16 @@ G=4*np.pi**2
 SIMULATION_LENGTH=10
 STEP=1/365
 TIME=np.arange(0,SIMULATION_LENGTH,STEP)
-n=TIME.shape[0]
 eM=3.003e-6
 #Masses are in solar masses 1 sM= 2e30 kg, so 1 kg = 0.5e-30 sM
 #Lengths are in AU
 #Durations are in AU
 
 
-def position_func(apogee,perigee,argument,mass,phase=0):
+
+def position_func(apogee,perigee,argument,phase=0):
     if apogee*perigee==0:
-        return (lambda t:[np.zeros_like(t),np.zeros_like(t)]),mass
+        return lambda t:[np.zeros_like(t),np.zeros_like(t),np.zeros_like(t),np.zeros_like(t)]
     c=(apogee-perigee)/2
     a=(perigee+apogee)/2
     e=c/a
@@ -31,31 +31,33 @@ def position_func(apogee,perigee,argument,mass,phase=0):
     T=a**(2/3)
     angle=np.pi/180
     rot=np.array([[np.cos(angle),-np.sin(angle)],[np.sin(angle),np.cos(angle)]])
-    return lambda t:rot.dot(np.array([a*np.cos(2*np.pi*t/T+phase),b*np.sin(2*np.pi*t/T+phase)])),mass
+    return lambda t:np.concatenate((rot.dot(np.array([a*np.cos(2*np.pi*t/T+phase),b*np.sin(2*np.pi*t/T+phase)])),np.zeros(2)))
 
-earth=position_func(1.0167,0.98329,288.1,eM)
-mars=position_func(1.666,1.3814,286.5,0.107*eM)
+earth=position_func(1.0167,0.98329,288.1)
+mars=position_func(1.666,1.3814,286.5)
 sun=position_func(0,0,0,1)
 
-planets=[sun,earth,mars]
+PLANETS={sun:1,mars:0.107*eM,earth:eM}
 
-init=np.concatenate((earth[0](0)+np.array([-0.1,0]),(earth[0](STEP)-earth[0](0))*20))
+def acceleration(phasevec,t):
+    dphasevec=np.zeros(4)
+    for p in PLANETS.keys():# p est mtn la fonction qui a t associe la position d'une planete
+        phaseplan=p(t)
+        diff=phasevec-phaseplan
+        D=diff[:2]
+        R=(D.dot(D))**(3/2)
+        dphasevec[2:]+=G*PLANETS[p]*D/R
+    dphasevec[:2]=phasevec[2:]
+    return dphasevec
 
-def acc(phasevec,t):
-    pos=phasevec[:2]
-    FORCE=np.zeros(2)
-    for p in planets:
-        r=(p[0](t)-pos)
-        D=(r.dot(r))**0.5
-        assert D>0.01
-        FORCE+=G*p[1]*r/D**3
-    return np.concatenate((phasevec[2:],FORCE*STEP))
-
-R=integ.odeint(acc,init,TIME)
-
-x,y=R.T[:2]
-plt.plot(x,y)
-for p in planets:
-    x,y=p[0](TIME)
+def plot(phase):
+    x,y=phase.T[:2]
     plt.plot(x,y)
-    plt.plot(x[-1],y[-1],"o")
+    plt.plot(x[-1],y[-1],"x")
+    for p in PLANETS.keys():
+        p(TIME)
+        plt.plot()
+
+init=earth(0)*(1+0.2*np.random.random())
+
+phase=integ.odeint(acceleration,init,TIME)
